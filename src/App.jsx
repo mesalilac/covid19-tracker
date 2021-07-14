14a109feec082664b767312c654be4b45f1beebe
senpai-10 <bmjfdrh@gmail.com>
2021-07-14 05:17:30 +0300

    src/

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      
+      {/* side bar */}
+      <div id="sidebar"></div>
+
+      {/* main list */}
+      <div id="main"></div>
+    </div>
+  );
+}
+
+export default App;
