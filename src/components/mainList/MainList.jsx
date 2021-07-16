5c93fef9468e4b981107d981c90b269c8d0e48d2
senpai-10 <bmjfdrh@gmail.com>
2021-07-16 12:57:31 +0300

    table

--- src/components/mainList/MainList.jsx
+++ src/components/mainList/MainList.jsx
@@ -2,8 +2,38 @@ import "./index.css"
 
 function MainList() {
     return (
-        <div id="mainlist">
-            <p>hiiii</p>
+        <div>
+            <table id="data-table">
+                <thead>
+                    <tr>
+                        <th>Country</th>
+                        <th>Total Cases</th>
+                        <th>Total Recovered</th>
+                        <th>Total Deaths</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Country</td>
+                        <td>900</td>
+                        <td>200</td>
+                        <td>80</td>
+                    </tr>
+                    <tr>
+                        <td>Country</td>
+                        <td>900</td>
+                        <td>200</td>
+                        <td>80</td>
+                    </tr>
+                    <tr>
+                        <td>Country</td>
+                        <td>900</td>
+                        <td>200</td>
+                        <td>80</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
