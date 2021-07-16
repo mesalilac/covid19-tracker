afe67ea1e42581f723eced892531e26a2267e194
senpai-10 <bmjfdrh@gmail.com>
2021-07-16 11:44:46 +0300

    src/components/

---
+++ src/components/mainList/MainList.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+function MainList() {
+    return (
+        <div id="mainlist">
+            <p>hiiii</p>
+        </div>
+    )
+}
+
+export default MainList
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,9 @@
+import "./index.css"
+
+function SideBar() {
+  return (
+    <div id="sidebar"></div>
+  )
+}
+
+export default SideBar
