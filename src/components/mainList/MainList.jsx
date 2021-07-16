0b806972df1a4118e273d5704d0f92ef1d85887b
senpai-10 <bmjfdrh@gmail.com>
2021-07-16 13:00:16 +0300

    format

--- src/components/mainList/MainList.jsx
+++ src/components/mainList/MainList.jsx
@@ -1,41 +1,40 @@
 import "./index.css"
 
 function MainList() {
-    return (
-        <div>
-            <table id="data-table">
-                <thead>
-                    <tr>
-                        <th>Country</th>
-                        <th>Total Cases</th>
-                        <th>Total Recovered</th>
-                        <th>Total Deaths</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    <tr>
-                        <td>Country</td>
-                        <td>900</td>
-                        <td>200</td>
-                        <td>80</td>
-                    </tr>
-                    <tr>
-                        <td>Country</td>
-                        <td>900</td>
-                        <td>200</td>
-                        <td>80</td>
-                    </tr>
-                    <tr>
-                        <td>Country</td>
-                        <td>900</td>
-                        <td>200</td>
-                        <td>80</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div>
+      <table id="data-table">
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Total Cases</th>
+            <th>Total Recovered</th>
+            <th>Total Deaths</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Country</td>
+            <td>900</td>
+            <td>200</td>
+            <td>80</td>
+          </tr>
+          <tr>
+            <td>Country</td>
+            <td>900</td>
+            <td>200</td>
+            <td>80</td>
+          </tr>
+          <tr>
+            <td>Country</td>
+            <td>900</td>
+            <td>200</td>
+            <td>80</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default MainList
