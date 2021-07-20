a2ab4056aa95bc3770c11e2885d6033451c04ea5
senpai-10 <bmjfdrh@gmail.com>
2021-07-20 14:19:04 +0300

    globalData

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,8 +1,15 @@
 import "./index.css"
 import useFetch from "react-fetch-hook"
+import millify from "millify"
 
 function SideBar() {
   const {data} = useFetch("https://covid19.mathdro.id/api");
+  
+  const globalData = {
+    "confirmed": millify(data?.confirmed.value, {lowercase: true}),
+    "recovered": millify(data?.recovered.value, {lowercase: true}),
+    "deaths": millify(data?.deaths.value, {lowercase: true})
+  }
   
   return (
     <div id="sidebar">
